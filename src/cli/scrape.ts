87d3b88b5d878b60e404ce80735b5a6a71a2ec7e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-12 21:59:48 +0200

    feat(scrape-script): find all links for url

--- src/cli/scrape.ts
+++ src/cli/scrape.ts
@@ -1,6 +1,9 @@
 import colors from 'colors';
+import { formatDistanceToNowStrict } from 'date-fns';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
+
+import Browser from '../services/browser';
 
 type Args = {
   website?: string;
@@ -14,3 +17,18 @@ if (!website) {
 }
 
 console.log(colors.yellow(`${colors.bold('website:')} ${website}`));
+
+(async () => {
+  const start = new Date();
+  await Browser.init();
+  await Browser.browseUrl(website);
+  const links = await Browser.getLinks();
+
+  console.log(
+    colors.green(
+      `Found ${links.length} links in ${formatDistanceToNowStrict(start)}`,
+    ),
+  );
+
+  await Browser.close();
+})();
