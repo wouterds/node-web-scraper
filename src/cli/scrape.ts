22a61af1afa3559a751ba7d14e22b6854a484121
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-12 22:12:21 +0200

    feat(scrape-script): scrape links recursively

--- src/cli/scrape.ts
+++ src/cli/scrape.ts
@@ -18,17 +18,37 @@ if (!website) {
 
 console.log(colors.yellow(`${colors.bold('website:')} ${website}`));
 
+const data: Record<string, string> = {};
+const scrapeLinkRecuversively = async (url: string) => {
+  console.log(colors.magenta(`Scraping ${url}`));
+
+  await Browser.browseUrl(url);
+
+  for (const link of await Browser.getLinks()) {
+    if (!data[link]) {
+      data[link] = await Browser.getContent();
+      await scrapeLinkRecuversively(link);
+    }
+  }
+};
+
 (async () => {
   const start = new Date();
   await Browser.init();
-  await Browser.browseUrl(website);
-  const links = await Browser.getLinks();
+
+  await scrapeLinkRecuversively(website);
 
   console.log(
     colors.green(
-      `Found ${links.length} links in ${formatDistanceToNowStrict(start)}`,
+      `Found ${Object.keys(data).length} links in ${formatDistanceToNowStrict(
+        start,
+      )}`,
     ),
   );
+
+  for (const [link, content] of Object.entries(data)) {
+    console.log(`${colors.blue(link)} - ${colors.white(`${content.length}`)}`);
+  }
 
   await Browser.close();
 })();
